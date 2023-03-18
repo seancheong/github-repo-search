bb538909c5adf366f9476cc663d25b9e97602aac
Sean Cheong Zhen Xiong <sean21cheong@msn.com>
2023-03-18 16:56:11 +0900

    Remove redundant codes

--- src/services/utilService.test.ts
+++ src/services/utilService.test.ts
@@ -1,53 +1,6 @@
-import { throttle, formatDateTime } from './utilService';
+import { formatDateTime } from './utilService';
 
 describe('util service', () => {
-  describe('throttle', () => {
-    beforeAll(() => {
-      jest.useFakeTimers();
-    });
-
-    test('calls the passed in function immediately on the first time', () => {
-      // given
-      const fn = jest.fn();
-      const throttleFn = throttle(fn);
-      const arg = 'test';
-
-      // when
-      throttleFn(arg);
-
-      // then
-      expect(fn).toHaveBeenCalled();
-      expect(fn).toHaveBeenCalledWith(arg);
-    });
-
-    test('calls the passed in function after an interval on the second time', () => {
-      // given
-      const fn = jest.fn();
-      const throttleFn = throttle(fn);
-      const arg1 = { title: 'hello' };
-      const arg2 = { title: 'world' };
-
-      // when
-      throttleFn(arg1);
-      throttleFn(arg2);
-
-      // then
-      expect(fn).toHaveBeenCalledTimes(1);
-      expect(fn).toHaveBeenCalledWith(arg1);
-
-      jest.advanceTimersByTime(300);
-      expect(fn).toHaveBeenCalledTimes(1);
-
-      jest.advanceTimersByTime(300);
-      expect(fn).toHaveBeenCalledTimes(2);
-      expect(fn).toHaveBeenCalledWith(arg2);
-    });
-
-    afterAll(() => {
-      jest.useRealTimers();
-    });
-  });
-
   describe('getLocalizedDateTime', () => {
     beforeAll(() => {
       jest.useFakeTimers().setSystemTime(new Date('2023-03-18T09:00:00Z'));
--- src/services/utilService.ts
+++ src/services/utilService.ts
@@ -7,45 +7,6 @@ import {
 } from 'date-fns';
 
 /**
- * Higher order function of throttling
- * It's accepting a function as the first parameter,
- * and it will limit the number of times of that function
- * is called during an interval
- *
- * @param fn (function)
- * @param interval (milliseconds)
- * @returns generic return type of the passed in function
- */
-export const throttle = <T, K extends unknown[]>(
-  fn: (...args: K) => T,
-  interval = 500
-) => {
-  let shouldWait = false;
-  let waitingArgs: K | null = null;
-  const timeoutCallback = () => {
-    if (waitingArgs === null) {
-      shouldWait = false;
-    } else {
-      fn(...waitingArgs);
-      waitingArgs = null;
-      setTimeout(timeoutCallback, interval);
-    }
-  };
-
-  return (...args: K) => {
-    if (shouldWait) {
-      waitingArgs = args;
-      return;
-    }
-
-    fn(...args);
-    shouldWait = true;
-
-    setTimeout(timeoutCallback, interval);
-  };
-};
-
-/**
  * Format date/time to easily understand string
  *
  * @param dateTimeString (string)
